572dcc4e3d72463734bad990c20c3433479121aa
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2025-01-23 15:23:01 +0000

    1/5

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -1,9 +1,19 @@
 package lastpencil;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        System.out.println("Hello World!");
+// Specify the number of pencils
+        int numberOfPencils = 8; // You can change this number for different outputs
 
+        // Create a string of vertical bars representing pencils
+        String pencils = "|".repeat(numberOfPencils);
+
+        // Print the pencils and the "Your turn!" message
+        System.out.println(pencils);
+        System.out.println("Your turn!");
 
     }
 }
