840ce27d6e8ca6a7715709ced96ebf3c5423a9fd
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2025-01-24 13:55:27 +0000

    2/5

--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -4,16 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello World!");
-// Specify the number of pencils
-        int numberOfPencils = 8; // You can change this number for different outputs
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask for the number of pencils
+        System.out.println("How many pencils would you like to use:");
+        int numberOfPencils = scanner.nextInt();
+
+        // Clear the newline character from the input buffer
+        scanner.nextLine();
+
+        // Ask for the name of the player who goes first
+        System.out.println("Who will be the first (John, Jack):");
+        String firstPlayer = scanner.nextLine();
 
         // Create a string of vertical bars representing pencils
         String pencils = "|".repeat(numberOfPencils);
 
-        // Print the pencils and the "Your turn!" message
+        // Print the pencils and the first player's name
         System.out.println(pencils);
-        System.out.println("Your turn!");
+        System.out.println(firstPlayer + " is going first!");
 
+        // Close the scanner
+        scanner.close();
     }
 }
