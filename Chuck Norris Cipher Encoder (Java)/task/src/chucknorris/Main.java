3048a3643a3a1dd6f216acec4daa892a5a7a4891
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2025-01-26 09:49:24 +0000

    1/5

--- Chuck Norris Cipher Encoder (Java)/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder (Java)/task/src/chucknorris/Main.java
@@ -1,7 +1,20 @@
 package chucknorris;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input string:");
+//        System.out.println("> ");
+        String input = scanner.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            System.out.print(input.charAt(i));
+            if (i< input.length() -1) {
+                System.out.print(" ");
+            }
+        }
+        scanner.close();
     }
 }
