b4e966aa5b76b286c5b9eb089777f0dfbfe8d89c
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2025-01-27 13:28:17 +0000

    3/5

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -1,5 +1,7 @@
 package asciimirror;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,22 +9,21 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Input the file path:");
         String filePath = scanner.nextLine();
-        System.out.println(filePath);
 
-        List<String> cowAscii = cowAscii();
-        cowAscii.forEach(System.out::println);
-    }
-
-    private static List<String> cowAscii() {
-        return List.of(
-                "            ^__^",
-                "    _______/(oo)",
-                "/\\/(       /(__)",
-                "   | w----||    ",
-                "   ||     ||    "
-        );
+        File file = new File(filePath);
+        if (!file.exists() || !file.isFile()) {
+            System.out.println("File not found!");
+        } else {
+            try (Scanner fileScanner = new Scanner(file)) {
+                while (fileScanner.hasNextLine()) {
+                    String line = fileScanner.nextLine();
+                    System.out.println(line);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found!");
+            }
+        }
     }
 }
