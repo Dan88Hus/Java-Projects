ebc9bd5ff447705a333c1b5c9b6aa3053c3239da
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2025-01-29 14:45:59 +0000

    2/7

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,19 +1,47 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final String SECRET_CODE = "9305";
+    
     public static void main(String[] args) {
-        System.out.println("The secret code is prepared: ****.\n");
-        printTurn(1, "1234", "1 cow.");
-        printTurn(2, "5678", "1 bull.");
-        printTurn(3, "9012", "2 bulls.");
-        printTurn(4, "9083", "3 bulls.");
-        printTurn(5, "9084", "4 bulls.");
-        System.out.println("Congrats! The secret code is 9084.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a 4-digit number:");
+        String guess = scanner.next();
+        scanner.close();
+
+        gradeGuess(guess);
+        
     }
 
-    private static void printTurn(int turn, String answer, String grade) {
-        System.out.println("Turn " + turn + ". Answer:");
-        System.out.println(answer);
-        System.out.println("Grade: " + grade + "\n");
+    private static void gradeGuess(String guess) {
+        int bulls = 0, cows = 0;
+
+        for (int i = 0; i < SECRET_CODE.length(); i++) {
+            if (guess.charAt(i) == SECRET_CODE.charAt(i)) {
+                bulls++;
+            } else if (SECRET_CODE.contains(String.valueOf(guess.charAt(i)))) {
+                cows++;
+            }
+        }
+        System.out.print("Grade: ");
+        if (bulls == 0 && cows == 0) {
+            System.out.print("None");
+        } else {
+            if (bulls > 0) {
+                System.out.print(bulls + " bull(s)");
+            }
+            if (bulls > 0 && cows > 0) {
+                System.out.print(" and ");
+            }
+            if (cows > 0) {
+                System.out.print(cows + " cow(s)");
+            }
+        }
+        System.out.println(". The secret code is " + SECRET_CODE + ".");
+
+
     }
 }
