2a7ea95f3231e2ee666726b4e91695c2c0e61c7e
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2025-02-23 15:01:34 +0000

    1/4

--- Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/task/src/editor/TextEditor.java
@@ -4,9 +4,17 @@ import javax.swing.*;
 
 public class TextEditor extends JFrame {
     public TextEditor() {
+        setTitle("Text Editor");
+        // Initialize the text area
+        JTextArea textArea = new JTextArea();
+        textArea.setName("TextArea");  // Required name for testing
+        // Make the text area fill most of the window
+        textArea.setBounds(10, 10, 280, 280);
+
+        add(textArea);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
-        setVisible(true);
         setLayout(null);
+        setVisible(true);
     }
 }
