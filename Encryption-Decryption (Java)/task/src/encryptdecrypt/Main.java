ceac28c8063307b0552bcb5fbfd1df91aa730da4
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2025-03-15 14:20:43 +0000

    2/6

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,18 +1,27 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String message = "we found a treasure!";
-        String encrypted = encrypt(message);
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the input message
+        String message = scanner.nextLine();
+        // Read the shift value (key)
+        int key = scanner.nextInt();
+
+        String encrypted = encrypt(message, key);
         System.out.println(encrypted);
+        scanner.close();
     }
 
-    private static String encrypt(String message) {
+    private static String encrypt(String message, int key) {
         StringBuilder result = new StringBuilder();
 
         for (char c : message.toCharArray()){
             if (c>= 'a' && c<='z'){
-                char encryptedChar = (char) ('a' + 'z' -c);
+                char encryptedChar = (char) ('a' + (c - 'a' + key) % 26);
                 result.append(encryptedChar);
             } else {
                 result.append(c);
